f5015665dbafd602ec1f6f63ca27696f2847327a
John Doe <johndoe@example.com>
2018-01-12 20:19:14 +0800

    第一天完成

--- src/component/App.vue
+++ src/component/App.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>测试环境</div>
+  <div>
+      公共头部
+      <router-view></router-view>
+      公共底部
+  </div>
 </template>
 <style>
-  
+
 </style>
 <script>
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import Vue from "vue";
-import app from "./component/App.vue"
+
+import app from "./component/App.vue";
+
+import vueRouter from "vue-router";
+Vue.use(vueRouter);
+import routerInfo from "./router/index.js"
+
+import axios from "axios";
+axios.defaults.baseURL="http://157.122.54.189:9095";
+Vue.prototype.$http=axios;
+
+import api from "./js/api-config"
+Vue.prototype.api=api;
 
 new Vue({
   el:"#app",
-  render:function(createElement){
-    return createElement(app)
-  }
+  render:createElement=>createElement(app),
+  router:new vueRouter(routerInfo)
 })
